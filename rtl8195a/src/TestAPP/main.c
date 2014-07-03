211426950110b4f984b1d8f78be0e5675cbf47ed
chrisyang <yangkunming99@gmail.com>
2014-07-03 20:37:01 +0800

    37

--- rtl8195a/src/TestAPP/main.c
+++ rtl8195a/src/TestAPP/main.c
@@ -97,7 +97,7 @@ static void cmd_wifi_connect(int argc, char **argv)
 	printf("Joining BSS ...\n\r");
 	
 //todo: send relative data to Ameba by using the module inic_8195a.ko
-		strcpy(cmdDesc->cmdtype, SDIO_CMD_wifi_connect);	
+		strcpy(cmdDesc.cmdtype, SDIO_CMD_wifi_connect);	
 //		cmdDesc.cmdtype = SDIO_CMD_wifi_connect;
 		cmdDesc.datatype = MNGMT_FRAME;
 		cmdDesc.offset = sizeof(CMD_DESC);
@@ -106,7 +106,7 @@ static void cmd_wifi_connect(int argc, char **argv)
 	
 //		pData->cmd = cmdDesc;
 //		cmdtype = 'T0';
-		printf("pData->cmd.cmdtype is: 0x%x\n\r", SDIO_CMD_wifi_connect);
+		printf("pData->cmd.cmdtype is: %s\n\r", SDIO_CMD_wifi_connect);
 //		memcpy(&(pData->cmd), &cmdDesc, sizeof(cmdDesc));
 //		memcpy(pData->cmd_data, cmd_buf, sizeof(cmd_buf));
 //		printf("pData->cmd_data: %s\n\r", pData->cmd_data);
