4479c90718222052fb3b366d5d68a4cd22dc3f1b
chrisyang <yangkunming99@gmail.com>
2014-07-03 20:42:54 +0800

    40

--- rtl8195a/src/TestAPP/main.c
+++ rtl8195a/src/TestAPP/main.c
@@ -102,9 +102,9 @@ static void cmd_wifi_connect(int argc, char **argv)
 		cmdDesc.offset = sizeof(CMD_DESC);
 		cmdDesc.pktsize = sizeof(cmd_buf);
 	
-		pData->cmd = cmdDesc;
+
+		memcpy(&(pData->cmd), &cmdDesc, sizeof(cmdDesc));
 		printf("pData->cmd.cmdtype is: %s\n\r", pData->cmd.cmdtype);
-//		memcpy(&(pData->cmd), &cmdDesc, sizeof(cmdDesc));
 //		memcpy(pData->cmd_data, cmd_buf, sizeof(cmd_buf));
 //		printf("pData->cmd_data: %s\n\r", pData->cmd_data);
 
