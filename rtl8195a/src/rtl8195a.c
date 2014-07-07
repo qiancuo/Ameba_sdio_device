9586c83507d0b1dd6aefad942779c24a335147cc
chrisyang <yangkunming99@gmail.com>
2014-07-07 10:51:44 +0800

    2

--- rtl8195a/src/rtl8195a.c
+++ rtl8195a/src/rtl8195a.c
@@ -87,7 +87,7 @@ static ssize_t myFunc_Close(struct inode *inode, struct file *file)
 	return 0;
 }
 
-static int myFunc_ioctl(struct inode *inode, struct file *file, unsigned int cmd, unsigned long arg)
+static long myFunc_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
 	printk(KERN_DEBUG "%s():\n", __FUNCTION__);
 	return 0;
@@ -376,8 +376,9 @@ static struct file_operations fops =
 	write : myFunc_Write,
 	open : myFunc_Open,
 	release : myFunc_Close,
-	ioctl : myFunc_ioctl
+	unlocked_ioctl : myFunc_ioctl
 };
+
 
 static const struct sdio_device_id sdio_ids[] =
 {
