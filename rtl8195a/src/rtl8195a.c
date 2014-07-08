57b3011bef1e1463ded9ccb5a2f0c0698820f4ec
chrisyang <yangkunming99@gmail.com>
2014-07-08 13:03:41 +0800

    42

--- rtl8195a/src/rtl8195a.c
+++ rtl8195a/src/rtl8195a.c
@@ -154,7 +154,7 @@ static ssize_t myFunc_Write(struct file *file, const char *buf, size_t count, lo
 		 printk(KERN_DEBUG "copy from user failed!\n"); 
 		 return -EFAULT;
 	  }
-	printk("g_SDIO_cmdData length is %d\n", strlen((char *)&g_SDIO_cmdData));
+	printk("g_SDIO_cmdData length is %d\n", strlen((char *)g_SDIO_cmdData));
 //		pwlan_cmd = (PCMD_DESC)g_SDIO_cmdData;
 //		if(pwlan_cmd->datatype == 1)
 //		{
