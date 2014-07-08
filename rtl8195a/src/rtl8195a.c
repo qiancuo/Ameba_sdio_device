42e2f9d74ab07d3db6241ca4fe0ab4cdd388f5b6
chrisyang <yangkunming99@gmail.com>
2014-07-08 10:45:45 +0800

    33

--- rtl8195a/src/rtl8195a.c
+++ rtl8195a/src/rtl8195a.c
@@ -142,14 +142,14 @@ static ssize_t myFunc_Write(struct file *file, const char *buf, size_t count, lo
 {
 	printk(KERN_DEBUG "%s():\n", __FUNCTION__);
 //	memset(g_SDIO_cmdData, 0, sizeof(g_SDIO_cmdData)); 
-	CMD_DESC wlan_cmd;
+	PCMD_DESC pwlan_cmd;
 	if(copy_from_user(g_SDIO_cmdData,buf,sizeof(g_SDIO_cmdData)))
 	 {
 		 printk(KERN_DEBUG "copy from user failed!\n"); 
 		 return -EFAULT;
 	  }
-	wlan_cmd = (CMD_DESC)g_SDIO_cmdData;
-	if(wlan_cmd.datatype == 0)
+	pwlan_cmd = (PCMD_DESC)g_SDIO_cmdData;
+	if(pwlan_cmd->datatype == 0)
 	{
 		SendOnePkt(gHal_Data->func);
 	}
