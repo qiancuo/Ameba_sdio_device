796122cdf861083edd7f64bb82241fbe7926178b
chrisyang <yangkunming99@gmail.com>
2014-07-10 16:41:05 +0800

    42

--- rtl8195a/src/osdep/linux/osdep_service_linux.c
+++ rtl8195a/src/osdep/linux/osdep_service_linux.c
@@ -17,3 +17,44 @@ void _rtw_memcpy(void* dst, void* src, u32 sz)
 {
 	memcpy(dst, src, sz);
 }
+
+void DumpForOneBytes(IN u8 *pData, IN u8 Len)
+{
+	u8 *pSbuf = pData;	
+	s8 Length=Len;
+
+	s8 LineIndex=0,BytesIndex,Offset;
+	printk("\r [Addr]   .0 .1 .2 .3 .4 .5 .6 .7 .8 .9 .A .B .C .D .E .F\r\n" );
+
+	while( LineIndex< Length)
+	{		
+			printk("%08X: ", (pSbuf+LineIndex) );
+
+			if(LineIndex+16 < Length)
+				Offset=16;
+			else			
+				Offset=Length-LineIndex;
+			
+
+			for(BytesIndex=0; BytesIndex<Offset; BytesIndex++)
+				printk("%02x ", pSbuf[LineIndex+BytesIndex]);	
+
+			for(BytesIndex=0;BytesIndex<16-Offset;BytesIndex++)	//a last line
+    			printk("   ");
+
+
+			printk("    ");		//between byte and char
+			
+			for(BytesIndex=0;  BytesIndex<Offset; BytesIndex++) {
+                
+				if( ' ' <= pSbuf[LineIndex+BytesIndex]  && pSbuf[LineIndex+BytesIndex] <= '~')
+					printk("%c", pSbuf[LineIndex+BytesIndex]);
+				else
+					printk(".");
+			}
+            
+			printk("\n\r");
+			LineIndex += 16;
+	}
+	
+}
