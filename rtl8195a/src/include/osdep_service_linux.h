ee789bb7be9e698e46c43019828a84f085865e5e
chrisyang <yangkunming99@gmail.com>
2014-07-10 13:43:20 +0800

    20

--- rtl8195a/src/include/osdep_service_linux.h
+++ rtl8195a/src/include/osdep_service_linux.h
@@ -52,21 +52,4 @@
 	typedef struct semaphore	_mutex;
 #endif
 
-u8* _rtw_malloc(u32 sz)
-{
-	u8 *pbuf=NULL;
-	pbuf = kmalloc(sz, in_interrupt()?GFP_ATOMIC : GFP_KERNEL);
-
-	return pbuf;
-}
-
-void _rtw_mfree(u8 *pbuf, u32 sz)
-{
-	kfree(pbuf);
-}
-
-void _rtw_memcpy(void* dst, void* src, u32 sz)
-{
-	memcpy(dst, src, sz);
-}
 #endif
