65a0e8e07eff006d2642a2d7f813e46c7a764ca4
chrisyang <yangkunming99@gmail.com>
2014-07-15 17:02:25 +0800

    10

--- rtl8195a/src/include/sdio_ops.h
+++ rtl8195a/src/include/sdio_ops.h
@@ -12,6 +12,7 @@ u32 sdio_read32(struct sdio_func *func, u32 addr);
 u32 sdio_read_port(PHAL_DATA_TYPE pData, u32 addr, u32 cnt, u8 * mem);
 u32 sdio_write_port(struct sdio_func *func, u32 addr, u32 cnt, u8 *mem);
 s32 sdio_local_read(PHAL_DATA_TYPE pData,	u32	addr, u32 cnt, u8 *pbuf);
+void sd_int_hal(PHAL_DATA_TYPE pHalData);
 
 /**************************************************/
 #endif
