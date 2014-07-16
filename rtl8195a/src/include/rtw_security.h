51a56e6f8c3cf4c664d416f1df348bcbfd1bf05c
chrisyang <yangkunming99@gmail.com>
2014-07-16 21:45:17 +0800

    48

--- rtl8195a/src/include/rtw_security.h
+++ rtl8195a/src/include/rtw_security.h
@@ -22,9 +22,9 @@
 
 #include "autoconf.h"
 //#include "basic_types.h"
-
-#include "ieee80211.h"
 #include "wlan_bassdef.h"
+#include "ieee80211.h"
+
 
 #define _NO_PRIVACY_		0x0
 #define _WEP40_				0x1
