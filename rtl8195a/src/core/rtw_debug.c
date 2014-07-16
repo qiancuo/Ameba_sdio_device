a88481e98edb95f0afec772663a4c31a6d897cd0
chrisyang <yangkunming99@gmail.com>
2014-07-16 14:33:16 +0800

    11

--- rtl8195a/src/core/rtw_debug.c
+++ rtl8195a/src/core/rtw_debug.c
@@ -19,7 +19,7 @@
  ******************************************************************************/
 #define _RTW_DEBUG_C_
 
-#include "drv_types.h"
+#include <drv_types.h>
 
 u32 GlobalDebugLevel = _drv_err_;
 
