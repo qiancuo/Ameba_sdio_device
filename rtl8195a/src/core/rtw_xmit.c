f70a189cab6dbd19a0abc2f398a06174272d1e29
chrisyang <yangkunming99@gmail.com>
2014-07-18 14:52:28 +0800

    111

--- rtl8195a/src/core/rtw_xmit.c
+++ rtl8195a/src/core/rtw_xmit.c
@@ -24,6 +24,7 @@
 #include "../include/8195_desc.h"
 #include "../include/8195_sdio_reg.h"
 #include "../include/xmit_osdep.h"
+#include "../include/osdep_service.h"
 static s32 update_txdesc(_adapter *padapter, _pkt *pkt, TXDESC_8195A *ptxdesc)
 {
 	sint res = _SUCCESS;
