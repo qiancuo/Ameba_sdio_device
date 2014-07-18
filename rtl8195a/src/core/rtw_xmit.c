c670f4406a6646d056e46881881bb80097e5d213
chrisyang <yangkunming99@gmail.com>
2014-07-18 14:45:27 +0800

    108

--- rtl8195a/src/core/rtw_xmit.c
+++ rtl8195a/src/core/rtw_xmit.c
@@ -1,4 +1,27 @@
-
+/******************************************************************************
+ *
+ * Copyright(c) 2007 - 2012 Realtek Corporation. All rights reserved.
+ *                                        
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110, USA
+ *
+ *
+ ******************************************************************************/
+#define _RTW_XMIT_C_
+#include "../include/basic_types.h"
+#include "../include/drv_types.h"
+#include "../include/rtw_debug.h"
+#include "../include/8195_desc.h"
 static s32 update_txdesc(_adapter *padapter, _pkt *pkt, TXDESC_8195A *ptxdesc)
 {
 	sint res = _SUCCESS;
