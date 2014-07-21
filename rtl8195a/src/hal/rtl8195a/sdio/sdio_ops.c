6ed5a375627ca14201e6ab12dcd5da5aeea434cc
chrisyang <yangkunming99@gmail.com>
2014-07-21 23:38:37 +0800

    216

--- rtl8195a/src/hal/rtl8195a/sdio/sdio_ops.c
+++ rtl8195a/src/hal/rtl8195a/sdio/sdio_ops.c
@@ -471,7 +471,7 @@ _func_enter_;
 	
 //	if (claim_needed)
 		sdio_claim_host(pfunc);
-	err = _chris_sd_write(pfunc, addr, cnt, pdata);
+//	err = _chris_sd_write(pfunc, addr, cnt, pdata);
 //	if (claim_needed)
 		sdio_release_host(pfunc);
 _func_exit_;
