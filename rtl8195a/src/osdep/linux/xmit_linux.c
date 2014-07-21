cfb47ffabd4693388e8afe170a5dc01807983e76
chrisyang <yangkunming99@gmail.com>
2014-07-21 23:21:33 +0800

    211

--- rtl8195a/src/osdep/linux/xmit_linux.c
+++ rtl8195a/src/osdep/linux/xmit_linux.c
@@ -289,7 +289,7 @@ int rtw_xmit_entry(_pkt *pkt, _nic_hdl pnetdev)
 		_rtw_memcpy((pxmitbuf+txdesc.offset), pfile.cur_buffer, txdesc.txpktsize);
 		for(i=0;i<(txdesc.txpktsize+txdesc.offset);i++)
 			printk("pxmitbuf[%d] = 0x%02x\n", i, *(pxmitbuf+i));
-		chris_sdio_write_port(pfunc, WLAN_TX_HIQ_DEVICE_ID, (txdesc.txpktsize+txdesc.offset), pxmitbuf);
+//		chris_sdio_write_port(pfunc, WLAN_TX_HIQ_DEVICE_ID, (txdesc.txpktsize+txdesc.offset), pxmitbuf);
 		for(i=0;i<10000;i++);
 	}
 //	rtw_mfree(padapter->hw_init_completed, sizeof(u8));
