92e0bc3af2e23b120ac4edb3ce5b05eadf6108cf
chrisyang <yangkunming99@gmail.com>
2014-07-21 16:37:57 +0800

    169

--- rtl8195a/src/osdep/linux/xmit_linux.c
+++ rtl8195a/src/osdep/linux/xmit_linux.c
@@ -273,9 +273,9 @@ int rtw_xmit_entry(_pkt *pkt, _nic_hdl pnetdev)
 		pfile.pkt_len = pfile.buf_len = pkt->len;
 		txdesc.txpktsize = pkt->len;
 		TxDescGen(&txdesc, txdesc.txpktsize, 1);
-		_rtw_memcpy(pxmitbuf->pdata, &txdesc, txdesc.offset);
-		_rtw_memcpy((pxmitbuf->pdata+txdesc.offset), pkt->data, pkt->len);
-		sdio_write_port(pintfhdl, WLAN_TX_HIQ_DEVICE_ID, txdesc.txpktsize, pxmitbuf);
+//		_rtw_memcpy(pxmitbuf->pdata, &txdesc, txdesc.offset);
+//		_rtw_memcpy((pxmitbuf->pdata+txdesc.offset), pkt->data, pkt->len);
+//		sdio_write_port(pintfhdl, WLAN_TX_HIQ_DEVICE_ID, txdesc.txpktsize, pxmitbuf);
 	}
 	rtw_mfree(padapter->hw_init_completed, sizeof(u8));
 	rtw_mfree(pxmitbuf, sizeof(*pxmitbuf));
